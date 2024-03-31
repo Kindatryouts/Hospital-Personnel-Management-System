812875e6a80073f155a5abf3081fc337a1d29eb1
Yabsra Yihenew <yabsrayabsra2222@gmail.com>
2024-03-31 19:31:42 +0300

    Update HMS_source-code.cpp

--- Hospital Management system/HMS_source-code.cpp
+++ Hospital Management system/HMS_source-code.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include<iomanip>
+#include<fstream>
+#include<ctime>
+
+using namespace std;
+
+struct employee{
+    string name;
+    string id;
+    int age;
+    string gender;
+    string eduLevel;
+    string phoneNum;
+    string profession;
+    bool attendance;  // daily attendance
+    double performance;  //out of 10 or 100
+    int numofEmp;
+    string salary;
+    string workingSchedule; // include ward program + 8 hr per 24hr
+    string breakSchedule; // annual break 14 day for every emp
+    double numofAttendance ;
+    double numofWard;
+    double mngtScore; // score given by their managers
+
+};
 
